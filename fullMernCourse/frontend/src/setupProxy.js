603c6ba14a8885685c2acc8e899082840fcc6472
Liavfirstpro <liavbaron10@gmail.com>
2023-04-16 20:44:55 +0300

    x

--- fullMernCourse/frontend/src/setupProxy.js
+++ fullMernCourse/frontend/src/setupProxy.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
       target: "https://liav-notes-api.onrender.com",
+    //   target: "http://localhost:5000",
       changeOrigin: true,
     })
   );
